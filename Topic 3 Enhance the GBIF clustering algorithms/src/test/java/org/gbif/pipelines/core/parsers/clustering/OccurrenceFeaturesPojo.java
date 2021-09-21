48e2a25b064fd577e266820fdd9d3399b47aa7a7
timrobertson100 <timrobertson100@gmail.com>
2021-09-21 13:47:42 +0200

    Adds missing unit tests

---
+++ Topic 3 Enhance the GBIF clustering algorithms/src/test/java/org/gbif/pipelines/core/parsers/clustering/OccurrenceFeaturesPojo.java
@@ -0,0 +1,140 @@
+package org.gbif.pipelines.core.parsers.clustering;
+
+import lombok.Builder;
+
+/** A POJO implementation for simple tests. */
+@Builder
+public class OccurrenceFeaturesPojo implements OccurrenceFeatures {
+  private final String id;
+  private final String datasetKey;
+  private final String speciesKey;
+  private final String taxonKey;
+  private final String basisOfRecord;
+  private final Double decimalLatitude;
+  private final Double decimalLongitude;
+  private final Integer year;
+  private final Integer month;
+  private final Integer day;
+  private final String eventDate;
+  private final String scientificName;
+  private final String countryCode;
+  private final String typeStatus;
+  private final String occurrenceID;
+  private final String recordedBy;
+  private final String fieldNumber;
+  private final String recordNumber;
+  private final String catalogNumber;
+  private final String otherCatalogNumbers;
+  private final String institutionCode;
+  private final String collectionCode;
+
+  @Override
+  public String getId() {
+    return id;
+  }
+
+  @Override
+  public String getDatasetKey() {
+    return datasetKey;
+  }
+
+  @Override
+  public String getSpeciesKey() {
+    return speciesKey;
+  }
+
+  @Override
+  public String getTaxonKey() {
+    return taxonKey;
+  }
+
+  @Override
+  public String getBasisOfRecord() {
+    return basisOfRecord;
+  }
+
+  @Override
+  public Double getDecimalLatitude() {
+    return decimalLatitude;
+  }
+
+  @Override
+  public Double getDecimalLongitude() {
+    return decimalLongitude;
+  }
+
+  @Override
+  public Integer getYear() {
+    return year;
+  }
+
+  @Override
+  public Integer getMonth() {
+    return month;
+  }
+
+  @Override
+  public Integer getDay() {
+    return day;
+  }
+
+  @Override
+  public String getEventDate() {
+    return eventDate;
+  }
+
+  @Override
+  public String getScientificName() {
+    return scientificName;
+  }
+
+  @Override
+  public String getCountryCode() {
+    return countryCode;
+  }
+
+  @Override
+  public String getTypeStatus() {
+    return typeStatus;
+  }
+
+  @Override
+  public String getOccurrenceID() {
+    return occurrenceID;
+  }
+
+  @Override
+  public String getRecordedBy() {
+    return recordedBy;
+  }
+
+  @Override
+  public String getFieldNumber() {
+    return fieldNumber;
+  }
+
+  @Override
+  public String getRecordNumber() {
+    return recordNumber;
+  }
+
+  @Override
+  public String getCatalogNumber() {
+    return catalogNumber;
+  }
+
+  @Override
+  public String getOtherCatalogNumbers() {
+    return otherCatalogNumbers;
+  }
+
+  @Override
+  public String getInstitutionCode() {
+    return institutionCode;
+  }
+
+  @Override
+  public String getCollectionCode() {
+    return collectionCode;
+  }
+}
